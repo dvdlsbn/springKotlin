e343fa1ba0becc81f4d1e63b64aca1f4b7c2e05a
dvdlsbn <lisbona.david@gmail.com>
2024-07-02 22:32:21 +0200

    ok

--- app/src/main/kotlin/com/example/springkotlin/SpringkotlinApplication.kt
+++ app/src/main/kotlin/com/example/springkotlin/SpringkotlinApplication.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses=HtmlController::class.java)
+@ComponentScan(basePackageClasses=HtmlController ::class.java)
 class SpringkotlinApplication
 
 fun main(args: Array<String>) {
